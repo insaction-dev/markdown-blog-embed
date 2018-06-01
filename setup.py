6232864efcd6cb24af03d677ac89446d2ecedcbf
Nathan Graule <solarliner@gmail.com>
2018-06-01 16:23:33 +0200

    Add dependencies to setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='markdown-blog-embed',
+    version='1.0',
+    packages=['blog_embed'],
+    url='https://github.com/insaction-dev/markdown-blog-embed',
+    license='MIT',
+    author='Nathan "SolarLiner" Graule',
+    author_email='solarliner@gmail.com',
+    description='Custom embeds for the INSAction blog.',
+    install_requires=['markdown']
+)
